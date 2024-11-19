b61f0043e4f0ac83ddf8f7daf49717de1eba97ed
hbasrik <basri.kocagol@ug.bilkent.edu.tr>
2024-11-19 22:18:01 +0300

    building backend for deployment

--- backend/api/products.js
+++ backend/api/products.js
@@ -3,25 +3,21 @@ const products = require("./products.json");
 
 const goldPriceAPI = "https://api.metals.dev/v1/metal/spot?api_key=ZXXJN6XEWKBTSQEQHYLF771EQHYLF&metal=gold&currency=USD";
 
-app.get("/products", async (req, res) => {
-  try {
-    
-    const goldResponse = await axios.get(goldPriceAPI);
-    const goldPricePerTroyOunce = goldResponse.data.rate.price; 
-    
-    const goldPricePerGram = goldPricePerTroyOunce / 31.1035;
-
-    console.log(`Gold Price Per Gram: ${goldPricePerGram} USD`);
-
-   
-    const updatedProducts = products.map((product) => {
-      const price = (product.popularityScore + 1) * product.weight * goldPricePerGram;
-      return { ...product, price: price.toFixed(2) };
-    });
-
-    res.json(updatedProducts);
-  } catch (error) {
-    console.error("Error fetching gold price:", error.message);
-    res.status(500).json({ error: "Failed to fetch gold price" });
-  }
-});
+module.exports = async (req, res) => {
+    try {
+      const goldResponse = await axios.get(goldPriceAPI);
+      const goldPricePerTroyOunce = goldResponse.data.rate.price;
+      const goldPricePerGram = goldPricePerTroyOunce / 31.1035;
+  
+      const updatedProducts = products.map((product) => {
+        const price =
+          (product.popularityScore + 1) * product.weight * goldPricePerGram;
+        return { ...product, price: price.toFixed(2) };
+      });
+  
+      res.status(200).json(updatedProducts);
+    } catch (error) {
+      console.error("Error fetching gold price:", error.message);
+      res.status(500).json({ error: "Failed to fetch gold price" });
+    }
+  };
